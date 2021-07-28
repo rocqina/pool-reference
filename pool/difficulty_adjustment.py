2f0b17be467e851e9b22754c06bef7b40b415364
peng.guan <peng.guan@okg.com>
2021-07-28 17:41:57 +0800

    Adjust difficulty at least once a day (time_target).

--- pool/difficulty_adjustment.py
+++ pool/difficulty_adjustment.py
@@ -33,8 +33,16 @@ def get_new_difficulty(
         return max(min_difficulty, uint64(int(current_difficulty // 1.5)))
 
     # If we don't have enough partials at this difficulty, don't update yet
-    if len(recent_partials) < number_of_partials_target:
+    time_taken = uint64(recent_partials[0][0] - recent_partials[-1][0])
+
+    # If we don't have enough partials at this difficulty and time between last and
+    # 1st partials is below target time, don't update yet
+    if len(recent_partials) < number_of_partials_target and time_taken < time_target:
         return current_difficulty
+
+    # Adjust time_taken if number of partials didn't reach number_of_partials_target
+    if len(recent_partials) < number_of_partials_target:
+        time_taken = time_taken * number_of_partials_target / len(recent_partials)
 
     # Finally, this is the standard case of normal farming and slow (or no) growth, adjust to the new difficulty
     time_taken = uint64(recent_partials[0][0] - recent_partials[-1][0])
